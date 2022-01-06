45cedec827a2c79e9e5d7ee96667f34d667bb0c6
hoppingd <45049563+hoppingd@users.noreply.github.com>
2022-01-06 13:32:18 -0500

    funny archons traverse the map

--- src/newplayer/Archon.java
+++ src/newplayer/Archon.java
@@ -8,6 +8,7 @@ public class Archon extends MyRobot {
 
     int minersBuilt;
     int depositsDetected;
+    boolean arrived = false;
 
     public Archon(RobotController rc){
         super(rc);
@@ -18,8 +19,70 @@ public class Archon extends MyRobot {
     public void play(){
         if (minersBuilt < 1) {
             getMines();
+        };
+        if (rc.getMode() == RobotMode.TURRET) {
+            if (!arrived) {
+                if (minersBuilt > depositsDetected) {
+                    try {
+                        boolean HQ_DECIDED = (rc.readSharedArray(0) >> 12) == 1; //xxxx xxyy yyyy HQ_DECIDED
+                        if (!HQ_DECIDED) {
+                            System.err.println("wrote hq at " + rc.getLocation());
+                            MapLocation myLoc = rc.getLocation();
+                            int code = (1 << 12) + (myLoc.y << 6) + myLoc.x;
+                            rc.writeSharedArray(0, code);
+                            arrived = true;
+                        }
+                        else {
+                            System.err.println("hq should be going portable at " + rc.getLocation());
+                            if (rc.isTransformReady()) {
+                                rc.transform();
+                            }
+                        }
+                    } catch (Throwable t) {
+                        t.printStackTrace();
+                    }
+                }
+            }
+            tryBuild();
         }
-        tryBuild();
+        else {
+            if (arrived == false) {
+                tryMove();
+            }
+        }
+    }
+
+    void tryMove(){
+        MapLocation HQ = null;
+        try {
+            int HQloc = rc.readSharedArray(0); //xxxx xxyy yyyy HQ_DECIDED
+            int x = HQloc & 0x3F;
+            int y = (HQloc >> 6) & 0x3F;
+            HQ = new MapLocation(x,y);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        if (HQ != null){
+            if (rc.getLocation().isWithinDistanceSquared(HQ, 10)) {
+                try {
+                    if (rc.isTransformReady()) {
+                        rc.transform();
+                        arrived = true;
+                    }
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            }
+            else {
+                bfs.move(HQ);
+            }
+            return;
+        }
+        /*if (rc.getRoundNum() - birthday > exploreRounds){
+            if (goToEnemyHQ()) return;
+        }*/
+        return;
     }
 
     void tryBuild(){
@@ -36,7 +99,7 @@ public class Archon extends MyRobot {
                 }
             }
         }
-        else if (rc.getTeamLeadAmount(rc.getTeam()) > 100) { // arbitrary number
+        else if (rc.getTeamLeadAmount(myTeam) > RobotType.SOLDIER.buildCostLead) {
             for (Direction dir : Direction.allDirections()) {
                 try {
                     if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
@@ -46,6 +109,26 @@ public class Archon extends MyRobot {
                 } catch (Throwable t) {
                     t.printStackTrace();
                 }
+            }
+        }
+        else { // failed spawn, we'll try to heal
+            RobotInfo[] allies = rc.senseNearbyRobots(rc.getType().actionRadiusSquared, myTeam);
+            MapLocation bestLoc = null;
+            int lowestHP = 10000;
+            for (RobotInfo r : allies){
+                MapLocation allyLoc = r.getLocation();
+                if (rc.canRepair(r.getLocation())){
+                    int hp = r.getHealth();
+                    if (hp < lowestHP) {
+                        lowestHP = hp;
+                        bestLoc = allyLoc;
+                    }
+                }
+            }
+            try {
+                if (bestLoc != null) rc.repair(bestLoc);
+            } catch (Throwable t) {
+                t.printStackTrace();
             }
         }
     }
